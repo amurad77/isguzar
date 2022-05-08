98359237b2949a98f0034b844ebfa45387800b90
amurad77 <amurad7799@gmail.com>
2022-05-09 02:18:24 +0400

    pagination

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from core.models import Articles
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def home(request):
@@ -29,9 +30,26 @@ def news_detail(request, slug):
 
 
 def news(request):
+
+
     news = Articles.objects.all().order_by('-id')
 
+    page_num = request.GET.get('page', 1)
+
+    paginator = Paginator(news, 5) # 6 employees per page
+
+
+    try:
+        page_obj = paginator.page(page_num)
+    except PageNotAnInteger:
+        # if page is not an integer, deliver the first page
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        # if the page is out of range, deliver the last page
+        page_obj = paginator.page(paginator.num_pages)
+
     context = {
-        'news': news
+        'news': page_obj.object_list,
+        'page_obj': page_obj
     }
     return render (request, 'news.html', context)
