a1277aeb76b713fedfd3e5b9324852339210608c
amurad77 <amurad7799@gmail.com>
2022-05-17 20:38:12 +0400

    deploy

--- core/views.py
+++ core/views.py
@@ -130,7 +130,7 @@ def searchbar(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Mesajınız qeydə alındı')
-            return HttpResponseRedirect('/search?submitted=True')
+            return HttpResponseRedirect('/home?submitted=True')
             print('Form save')
         else:
             print('Form is invalid')
